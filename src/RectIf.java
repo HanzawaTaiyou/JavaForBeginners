28e1a25f06e0201b00038f36925317d001952f94
HanzawaTaiyou <zeal24360017@fsgcl.jp>
2025-05-26 12:16:51 +0900

    EX問題完成

--- src/RectIf.java
+++ src/RectIf.java
@@ -2,12 +2,14 @@
 public class RectIf extends MyFrame {
 	public void run() {
 		int x = 30;
-		setColor(0,128,0);
+		setColor(0,0,0);
 		int i;
-		for(i = 0; i < 9; i++) {
-			if(i == 4) {
-				fillRect(x,100,10,100);
+		for(i = 0; i < 20; i++) {
+			if(i < 10) {
+				setColor(x-30,x-30,x-30);
+				fillRect(x,80,10,100);
 			}else {
+				setColor(410-x,410-x,410-x);
 				fillRect(x,80,10,100);
 			}
 			x += 20;
