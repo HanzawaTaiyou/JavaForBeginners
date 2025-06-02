cf5a6c9fd3f1c420610f5af89f5d89eb46bcee4d
HanzawaTaiyou <zeal24360017@fsgcl.jp>
2025-06-02 11:18:48 +0900

    クラス作成

--- src/DisplayFaceObject.java
+++ src/DisplayFaceObject.java
@@ -5,5 +5,10 @@ public class DisplayFaceObject extends MyFrame{
 		face1.x=100;
 		face1.y=50;
 		face1.draw(this);
+		
+		Face face2=new Face();
+		face2.x=200;
+		face2.y=150;
+		face2.draw(this);
 	}
 }
