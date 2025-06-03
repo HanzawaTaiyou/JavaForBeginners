232b633366ceb4f4da91709b707cabf8bd6b8942
HanzawaTaiyou <zeal24360017@fsgcl.jp>
2025-06-03 10:49:31 +0900

    配列+for文で簡潔にした

--- src/DisplayFaceObject.java
+++ src/DisplayFaceObject.java
@@ -7,12 +7,12 @@ public class DisplayFaceObject extends MyFrame{
 		faces[2]=new Face(0,100,5,0);
 		for (int i=0; i <30; i++) {
 			clear();
-			faces[0].draw(this);
-			faces[1].draw(this);
-			faces[2].draw(this);
-			faces[0].move();
-			faces[1].move();
-			faces[2].move();
+			for (int j=0; j<3; j++) {
+				faces[j].draw(this);
+			}
+			for (int j=0; j<3; j++) {
+				faces[j].move();
+			}
 			sleep(0.1);
 		}
 	}
