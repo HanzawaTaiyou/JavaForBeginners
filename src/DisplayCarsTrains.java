aad088f0763327e87198b634e86794b0d5712475
HanzawaTaiyou <zeal24360017@fsgcl.jp>
2025-06-09 11:59:20 +0900

    練習問題8-1実行完成

--- src/DisplayCarsTrains.java
+++ src/DisplayCarsTrains.java
@@ -5,7 +5,10 @@ public class DisplayCarsTrains extends MyFrame{
 		Vector<Vehicle> vehicles=new Vector<Vehicle>();
 		vehicles.add(new Car(10,30,3,0));
 		vehicles.add(new Car(10,100,5,0));
-		vehicles.add(new Train(10,150,7,0));
+		vehicles.add(new Train(10,170,7,0));
+		vehicles.add(new Train(10,240,9,0));
+		vehicles.add(new Truck(10,310,11,0));
+		vehicles.add(new Truck(10,380,13,0));
 		for (int i=0; i<30; i++) {
 			clear();
 			for (int j=0; j<vehicles.size(); j++) {
